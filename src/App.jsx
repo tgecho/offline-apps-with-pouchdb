6af002733bd69abcecf3002dcbf4c7d1a90ecc31
Erik Simmler <tgecho@gmail.com>
2016-04-10 15:33:38 -0400

    Add server syncing

--- src/App.jsx
+++ src/App.jsx
@@ -19,22 +19,44 @@ export default React.createClass({
 				{docs: {...docs, [change.id]: change.doc}}
 			))
 		})
+
+		this.toggleSync()
 	},
 	render() {
+		const {docs, syncing} = this.state
+
 		return <div>
+			<button onClick={this.toggleSync}>{syncing ? 'Disconnect' : 'Connect'}</button>
+
 			<Doc key="new" save={this.saveDoc} />
 
-			{Object.keys(this.state.docs).map(id =>
+			{Object.keys(docs).map(id =>
 				<Doc doc={this.state.docs[id]} key={id} save={this.saveDoc} />
 			)}
 		</div>
 	},
 	componentWillUnmount() {
 		this._changes.cancel()
+		if (this.state.syncing) {
+			this.state.syncing.cancel()
+		}
 	},
 	saveDoc(doc) {
 		return doc._id
 			? this.props.db.put(doc)
 			: this.props.db.post(doc)
 	},
+	toggleSync() {
+		const {syncing} = this.state
+		if (syncing) {
+			this.setState({syncing: undefined}, () => syncing.cancel())
+		} else {
+			this.setState({
+				syncing: this.props.db.sync(this.props.remote, {
+					live: true,
+					retry: true,
+				})
+			})
+		}
+	}
 })
--- src/index.jsx
+++ src/index.jsx
@@ -10,5 +10,6 @@ import App from './App'
 window.PouchDB = PouchDB
 window.log = console.log.bind(console)
 window.db = new PouchDB('test')
+window.remote = new PouchDB('http://127.0.0.1:5984/test')
 
-ReactDOM.render(<App db={window.db} />, document.getElementById('app'))
+ReactDOM.render(<App db={window.db} remote={window.remote} />, document.getElementById('app'))
